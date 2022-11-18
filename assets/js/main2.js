da961af64f0dcd03e6c90d8b2f5dc78756ee8375
Jose Cannobbio <109194302+jtca94@users.noreply.github.com>
2022-11-18 16:53:01 -0300

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -2,7 +2,7 @@ const imagen = document.getElementById("img");
 //cambio de borde en imagen
 imagen.addEventListener("click", () => {
     let clase = imagen.className;
-    
+    // no es escalable pero para la tarea funciona
     if(clase == "w-25 mt-5 h-50 first") {
         imagen.className = "w-25 mt-5 h-50 second" ;
         return
